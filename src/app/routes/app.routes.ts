6ff204cce0b7a82c33b452412ed87eb22fe2edf9
JosiasAz <jjosias00@gmail.com>
2026-03-07 11:15:21 -0400

    fix: desenvolvimento de uma landing page oficial

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
-import { LandingPageComponent } from '../pages/landing-page/landing-page.component';
 
 export const routes: Routes = [
-    { path: '', component: LandingPageComponent },
+
 ];
